1b55e1968656018d5f8bf04d4de55fc06c9a2c83
Gio Lodi <giovanni.lodi42@gmail.com>
2021-06-08 06:45:37 +1000

    Generate new framework + unit tests

---
+++ AsyncExamples/AsyncExamples.h
@@ -0,0 +1,18 @@
+//
+//  AsyncExamples.h
+//  AsyncExamples
+//
+//  Created by Giovanni on 8/6/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AsyncExamples.
+FOUNDATION_EXPORT double AsyncExamplesVersionNumber;
+
+//! Project version string for AsyncExamples.
+FOUNDATION_EXPORT const unsigned char AsyncExamplesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AsyncExamples/PublicHeader.h>
+
+
