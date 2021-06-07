b1b2e998ce4e517be953f4895a31d6bf38b996dc
Gio Lodi <giovanni.lodi42@gmail.com>
2021-06-08 07:23:34 +1000

    Add XCTExpectFailure examples

--- AsyncExamplesTests/AsyncExamplesTests.swift
+++ AsyncExamplesTests/AsyncExamplesTests.swift
@@ -29,4 +29,24 @@ class AsyncExamplesTests: XCTestCase {
         // Doesn't compile:
         // > 'async' call in an autoclosure that does not support concurrency
     }
+
+    func testExpectFailure() async throws {
+        XCTExpectFailure("This test is expected to fail")
+
+        do {
+            let users = try await fetchUsers(count: 4)
+            // assertions here...
+        } catch {
+            XCTFail("Failed with \(error)")
+        }
+    }
+
+    func testExpectFailureWithMatcher() {
+        // Does not support async functions in the closure
+        XCTExpectFailure("This test is expected to fail") {
+            XCTAssertEqual(42, 24)
+        } issueMatcher: { issue in
+            return issue.compactDescription == #"XCTAssertEqual failed: ("42") is not equal to ("24")"#
+        }
+    }
 }
