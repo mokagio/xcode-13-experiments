f0bd755c8810d7c6425a803e0edf1a59effa5a06
Gio Lodi <giovanni.lodi42@gmail.com>
2021-06-08 07:06:59 +1000

    Add example of testing async/await code

--- AsyncExamplesTests/AsyncExamplesTests.swift
+++ AsyncExamplesTests/AsyncExamplesTests.swift
@@ -3,7 +3,24 @@ import XCTest
 
 class AsyncExamplesTests: XCTestCase {
 
-    func test() {
-        XCTAssertEqual(1, 1)
+    func testAsyncSuccess() async {
+        do {
+            let users = try await fetchUsers(count: 3)
+            XCTAssertEqual(users.count, 3)
+            XCTAssertTrue(users.contains("Ada"))
+            XCTAssertTrue(users.contains("Grace"))
+            XCTAssertTrue(users.contains("Margaret"))
+        } catch {
+            XCTFail("Failed with \(error)")
+        }
+    }
+
+    func testAsyncFailure() async {
+        do {
+            _ = try await fetchUsers(count: 4)
+            XCTFail("Expected async call to fail")
+        } catch {
+            XCTAssertEqual(error as? UserError, UserError.invalidCount)
+        }
     }
 }
