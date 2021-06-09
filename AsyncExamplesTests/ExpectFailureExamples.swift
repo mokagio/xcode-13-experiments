3b5f9c7e065d5af7ff88f29e5fa7867a6259ebe8
Gio Lodi <giovanni.lodi42@gmail.com>
2021-06-10 07:31:27 +1000

    Add more info in the XCTExpectFailure tests

--- AsyncExamplesTests/ExpectFailureExamples.swift
+++ AsyncExamplesTests/ExpectFailureExamples.swift
@@ -5,32 +5,28 @@ class ExpectFailureExamples: XCTestCase {
     let demoingFailures = false
 
     func testExpectFailure() {
-        XCTExpectFailure("Some reason why the failure is expected")
-        XCTAssertEqual(42, 24)
+        XCTExpectFailure("Failure expected till we fix #123")
+        XCTAssertEqual(computeAnswer(), 42)
     }
 
-    // You can use "strict" to fail the test if there's no failure.
-    func testExpectFailureStrict() throws {
-        try XCTSkipUnless(demoingFailures, "Skipping this test because it would fail since we're using strict mode")
-        XCTExpectFailure("Some reason why the failure is expected", strict: true)
-        // Equivalent to
-        let options = XCTExpectedFailure.Options()
-        options.isEnabled = true
-        options.isStrict = true
-        XCTExpectFailure("Some reason why the failure is expected", options: options)
+    // "strict" makes the test fail if there's no failure. It's `true` by default.
+    // Use `strict: false` to allow tests to pass.
+    func testExpectFailureNotStrict() throws {
+        XCTExpectFailure("This test is flaky. Will fix as part of #234", strict: false)
+        XCTAssertEqual(computeAnswer(), 42)
     }
 
     // You can use a closure to drill into the failure (`XCIssue`) that the test experienced to
-    // ensure it's the one you expect
+    // ensure it's the one you expect.
     func testExpectFailureDrillDown() {
-        XCTExpectFailure("Math is broken right now") { issue in
-            issue.compactDescription.contains(#"("42") is not equal to ("24")"#)
+        XCTExpectFailure("Failure expected till we fix #123") { issue in
+            issue.compactDescription.contains(#"("73") is not equal to ("42")"#)
         }
-        XCTAssertEqual(42, 24)
+        XCTAssertEqual(computeAnswer(), 42)
     }
 
-    // You can use it to handle code that throws, too
-
+    // `XCTExpectFailure` can also handle code that throws. Here's an example of code that would
+    // throw and fail the test.
     func testBrokenByCodeThatThrows() throws {
         try XCTSkipUnless(demoingFailures, "Skipping because this test demoes a failure")
 
@@ -39,24 +35,36 @@ class ExpectFailureExamples: XCTestCase {
         XCTAssertEqual(number + otherNumber, 42)
     }
 
+    // Using `XCTExpectFailure(_, failingBlock:)` you can handle expected failures in code that
+    // throws.
     func testExpectFailureWithAssert() throws {
-        let number = try XCTExpectFailure("How does this work?") {
+        let number = try XCTExpectFailure("Failure expected till we fix #123") {
             try codeThatThrows()
         }
         let otherNumber = 21
         XCTAssertEqual(number + otherNumber, 42)
     }
 
+    // `XCTExpectFailure(_, failingBlock:)` `rethrows` so it doesn't affect the control flow of
+    // the code under test. That means that you if the code doesn't end up throwing, you can use
+    // the result
     func testExpectFailureWithAssert_showingUnalteredControlFlow() throws {
-        let number = try XCTExpectFailure("How does this work?", options: .nonStrict()) {
+        let number = try XCTExpectFailure("Failure expected till we fix #123", options: .nonStrict()) {
             try codeThatThrowsButNotReally()
         }
         let otherNumber = 21
         XCTAssertEqual(number + otherNumber, 42)
     }
+
 }
 
 struct TestError: Error {}
+
+func computeAnswer() -> Int {
+    73
+    // Wrong result! Should be 42.
+    // See https://en.wikipedia.org/wiki/42_(number)#The_Hitchhiker's_Guide_to_the_Galaxy
+}
 
 func codeThatThrows() throws -> Int { throw TestError() }
 
